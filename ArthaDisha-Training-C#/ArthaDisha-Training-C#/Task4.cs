37935a15ccb5d8528bd1e038ef9079988deea6a2
Vswaroop07 <vyawahareswarup19@gmail.com>
2026-02-13 14:05:37 +0530

    (Task 4) DTOs (Data Transfer Objects)

---
+++ ArthaDisha-Training-C#/ArthaDisha-Training-C#/Task4.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArthaDisha_Training_C_
+{
+    internal class Task4
+    {
+        //DTO = DATA TRANSFER OBJECT
+        public class UserResponseDto0
+        {
+            public string Name { get; set; }
+            public string Email { get; set; }
+        }
+
+        //Request DTO (incoming data)
+        public class CreateUserRequestDto
+        {
+            public string Name { get; set; }
+            public string Email { get; set; }
+            public string Password { get; set; }
+        }
+
+        //Response DTO (outgoing data)
+        public class UserResponseDto
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Email { get; set; }
+        }
+
+
+        /*//Mapping (Manual Way)
+        var userDto = new UserResponseDto
+        {
+            Id = user.Id,
+            Name = user.Name,
+            Email = user.Email
+        };*/
+
+
+        //Validation with Data Annotations
+        public class ValidationCreateUserRequestDto
+        {
+            [Required]
+            [MaxLength(50)]
+            public string Name { get; set; }
+
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+
+            [Required]
+            [MinLength(6)]
+            public string Password { get; set; }
+        }
+
+
+
+    }
+}
