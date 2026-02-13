02e23160895b5299d745ff67f384b832170bdcb8
Vswaroop07 <vyawahareswarup19@gmail.com>
2026-02-13 13:07:20 +0530

    (Task 3) Collections

--- ArthaDisha-Training-C#/ArthaDisha-Training-C#/Program.cs
+++ ArthaDisha-Training-C#/ArthaDisha-Training-C#/Program.cs
@@ -17,10 +17,10 @@ class Program
         string name = "Swaroop";
 
         //Array Defination
-        int[] numbers = new int[3];
-        numbers[0] = 1;
-        numbers[1] = 2;
-        numbers[2] = 3;
+        int[] arr = new int[3];
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
 
         //Var, Dynamic
         var num = 89;
@@ -51,8 +51,41 @@ class Program
         Console.WriteLine(cs.Add(a, b));
         Console.WriteLine(cs.Add(c, d));
 
+        //List (DynamicArray)
+        List<int> listt = new List<int>();
+        listt.Add(10);
+        listt.Add(20);
+
+        //Dictionary(HashMap)
+        Dictionary<int, string> students = new Dictionary<int, string>();
+
+        students.Add(1, "Swaroop");
+        students.Add(2, "Ved");
+
+        Console.WriteLine(students[1]);
+
+
+        //HashSet Only Unique Values
+        HashSet<int> numbers = new HashSet<int>();
+        numbers.Add(1);
+        numbers.Add(1); // No Duplicates Allowed.
         
 
+        //Queue FIFO
+        Queue<string> queue = new Queue<string>();
 
-}
+        queue.Enqueue("Task1");
+        queue.Enqueue("Task2");
+
+        queue.Dequeue(); // removes Task1
+
+        //Stack LIFO
+        Stack<string> stack = new Stack<string>();
+
+        stack.Push("Page1");
+        stack.Push("Page2");
+
+        stack.Pop(); // removes Page2
+
+    }
 }
