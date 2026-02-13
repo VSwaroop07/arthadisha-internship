6a9ade1b244fd5821ac0ca47e1408867a82f2d12
Vswaroop07 <vyawahareswarup19@gmail.com>
2026-02-13 12:01:20 +0530

    (Task 1) C# Basic Study

--- ArthaDisha-Training-C#/ArthaDisha-Training-C#/Program.cs
+++ ArthaDisha-Training-C#/ArthaDisha-Training-C#/Program.cs
@@ -1,2 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Runtime.InteropServices;
+        //Enums
+        enum Status{
+            Pending,
+                Approved,
+                Rejected
+        }
+
+class Program
+{
+    static void Main()
+    {   
+        //variable defination
+        int age = 21;
+        string name = "Swaroop";
+
+        //Array Defination
+        int[] numbers = new int[3];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        numbers[2] = 3;
+
+        //Var, Dynamic
+        var num = 89;
+        dynamic x = 20;
+
+        //Nullable Value defination
+        int? o = null;
+
+        
+        Console.WriteLine("Hello, I am " + name + ". I am a Student. My Age is " + age);
+
+        string abc = name.Replace("a", "x");
+        Console.WriteLine(abc);
+    }
+}
